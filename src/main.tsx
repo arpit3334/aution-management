e1111acc5e40f9fd0a36f187edd228376426b6f0
Arpit Gupta <arpit3334@gmail.com>
2026-07-31 19:18:40 +0530

    Remove Vite configuration and entry files

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import AuctionModule from './AuctionModule';
-
-const container = document.getElementById('root');
-if (!container) throw new Error('#root element not found');
-createRoot(container).render(<AuctionModule />);
